3bf8a35198421eb047759d21ba26e10092148c00
NatKhomich <yalapulya94@mail.ru>
2023-01-28 11:54:21 +0600

    task 34

--- index.js
+++ index.js
@@ -243,8 +243,23 @@ function nbYear(p0, percent, aug, p) {
   }
   return totalYear;
 }
-//Задача 33 Убрать ! из строки 
+//Задача 33 Убрать ! из строки
 function removeExclamationMarks(s) {
   return s.replace(/!/g, "");
 }
-//
+//Задача 34 Вернуть нужную строку (лепестки)
+function howMuchILoveYou(nbPetals) {
+  if (nbPetals % 6 == 1) {
+    return "I love you";
+  } else if (nbPetals % 6 == 2) {
+    return "a little";
+  } else if (nbPetals % 6 == 3) {
+    return "a lot";
+  } else if (nbPetals % 6 == 4) {
+    return "passionately";
+  } else if (nbPetals % 6 == 5) {
+    return "madly";
+  } else if (nbPetals % 6 == 0) {
+    return "not at all";
+  }
+}
