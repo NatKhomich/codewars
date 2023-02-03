3eeafbc492a1705447b57c8442a986184e33bb10
NatKhomich <yalapulya94@mail.ru>
2023-02-03 15:36:36 +0600

    task64

--- index.js
+++ index.js
@@ -481,4 +481,18 @@ function sortByLength(array) {
 function isTriangle(a, b, c) {
   return a + b > c && b + c > a && c + a > b ? true : false;
 }
+//Задача 64 Вернуть true если yourPoints больше среднего арифметического массива classPoints
+function betterThanAverage(classPoints, yourPoints) {
+  let sum = 0;
+  for (let i = 0; i < classPoints.length; i++) {
+    sum += classPoints[i];
+  }
+  let average = sum / classPoints.length;
+  if (yourPoints > average) {
+    return true;
+  } else {
+    return false;
+  }
+}
 //
+
