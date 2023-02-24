c8d341830d6aacb9ea9c9fb29ddde28f4909f07c
NatKhomich <yalapulya94@mail.ru>
2023-02-24 13:01:04 +0600

    task 95

--- index.js
+++ index.js
@@ -978,4 +978,17 @@ function correct(string) {
 
 correct();
 
-//
+//Задача 95 Вывести оценку по вводным данным
+function finalGrade(exam, projects) {
+    if (exam > 90 || projects > 10) {
+        return 100;
+    } else if (exam > 75 && projects >= 5) {
+        return 90;
+    } else if (exam > 50 && projects >= 2) {
+        return 75;
+    } else {
+        return 0;
+    }
+}
+
+finalGrade();
